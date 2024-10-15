958b97d70ac201f35c0bd9a300324297cb2530c2
Trikest <sal.00000850311@unicap.br>
2024-10-15 00:09:43 -0300

    error

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.LoginRequestDTO;
+import com.example.demo.dto.RegisterRequestDTO;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.model.TipoUser;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.infra.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private final UserRepository repository;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private final TokenService tokenService;
+
+    public AuthController(UserRepository repository, PasswordEncoder passwordEncoder, TokenService tokenService) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body){
+        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+        if(passwordEncoder.matches(body.senha(), user.getSenha())) {
+            String token = this.tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDTO(user.getNome(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body){
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if(user.isEmpty()) {
+            User newUser = new User();
+            newUser.setSenha(passwordEncoder.encode(body.senha()));
+            newUser.setEmail(body.email());
+            newUser.setNome(body.nome());
+            newUser.setTipoUser(TipoUser.valueOf(String.valueOf(body.tipoUser())));
+            this.repository.save(newUser);
+
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getNome(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
---
+++ src/main/java/com/example/demo/service/infra/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service.infra;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String nome) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(nome).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getSenha(), new ArrayList<>());
+    }
+}
