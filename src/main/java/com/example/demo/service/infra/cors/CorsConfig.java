5d35cd7b4e68e6cd0870b2118e79a832c9eaf47e
victor.costa213 <132701394+victorcosta213@users.noreply.github.com>
2024-10-25 08:53:00 -0300

    update cors

--- src/main/java/com/example/demo/service/infra/cors/CorsConfig.java
+++ src/main/java/com/example/demo/service/infra/cors/CorsConfig.java
@@ -11,8 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000", "https://surd-tech-front-end.vercel.app")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*") 
-            .allowCredentials(true);
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+           // .allowedHeaders("*") 
+           // .allowCredentials(true);
     }
 }
